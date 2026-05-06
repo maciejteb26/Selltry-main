e42d7851da1d7ebfc8dc70cdbe21e308aa7e34ba
maciejteb26 <mteblowski@wp.pl>
2026-05-06 19:36:12 +0200

    Revert "first_commit_tests"
    This reverts commit 0fe3aeeab7659bc4b0d7d382c8709f483060f8ce.

--- server/src/jobs/index.ts
+++
@@ -1,7 +0,0 @@
-import { enqueuePublish, publishQueue, startPublishWorker } from './publish.job';
-
-export function startWorkers(): void {
-  startPublishWorker();
-}
-
-export { publishQueue, enqueuePublish };
--- server/src/routes/dashboard.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { authMiddleware } from '../middleware/auth.middleware';
-import * as listingCtrl from '../controllers/listing.controller';
-
-const router = Router();
-router.use(authMiddleware);
-router.get('/dashboard/stats', listingCtrl.getDashboardStats);
-
-export default router;
